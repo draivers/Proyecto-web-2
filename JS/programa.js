2c37671856912a2d30f5516e1ae84972548d9f49
draivers <61165239+draivers@users.noreply.github.com>
2022-03-09 11:49:15 -0500

    m

--- JS/programa.js
+++ JS/programa.js
@@ -10,8 +10,26 @@ function restablecercolor(){
 }
  
 function obtenerpareseimpares(){
+var cantidadpares =0;
+var cantidadimpares =0;
+var cantidadceros =0;
+var digito =0;
 var numero40 = parseInt(document.getElementById('numero40').value);
 if(numero40 < 1000 || numero40 > 9999){
     alert("el numero no es de 4 digitos, intente nuevamente");
 }
+else{
+         digito =parseInt (numero40/1000);
+        if(digito%2 === 0){
+            cantidadpares++;
+        }else{
+            if(digito%2 === 1){
+             cantidadimpares++;
+        }else{
+                cantidadceros++;
+            }
+        }
+         digito =parseInt (numero40)
+        
+        }
 }
