3cbbad556c4f4e99c6c2a95a0bc339b2997af8b3
draivers <61165239+draivers@users.noreply.github.com>
2022-03-09 11:04:09 -0500

    proyecto3

--- JS/programa.js
+++ JS/programa.js
@@ -11,4 +11,7 @@ function restablecercolor(){
  
 function obtenerpareseimpares(){
 var numeros = document.getElementById('numero40').valueOf;
+if(numero40 < 1000 && numero40 > 9999){
+    alert("el numero no es de 4 digitos, intente nuevamente")
+};
 }
