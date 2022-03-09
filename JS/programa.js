22d42b705cfbb2e92bbd6c390b5e2e9c780f18cf
draivers <61165239+draivers@users.noreply.github.com>
2022-03-09 10:59:53 -0500

    proyecto

--- JS/programa.js
+++ JS/programa.js
@@ -1,10 +1,14 @@
 function cambiarcolor(){
 var x =document.getElementById("titulo");
-x.style.color = "white";
-x.style.background = "black";
+x.style.color = 'white';
+x.style.background = 'black';
 }
 function restablecercolor(){
     var x =document.getElementById("titulo");
-    x.style.color = "black";
-    x.style.background = "bisque";
+    x.style.color = 'black';
+    x.style.background = 'bisque';
+}
+ 
+function obtenerpareseimpares(){
+var numeros = document.getElementById('numero40').valueOf;
 }
