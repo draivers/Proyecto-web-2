48e07a91af8e5985b429e7d50e6b0d1d2e536b7b
draivers <61165239+draivers@users.noreply.github.com>
2022-03-09 10:37:40 -0500

    proyecto

--- JS/programa.js
+++ JS/programa.js
@@ -1,1 +1,10 @@
-
+function cambiarcolor(){
+var x =document.getElementById("titulo");
+x.style.color = "white";
+x.style.background = "black";
+}
+function restablecercolor(){
+    var x =document.getElementById("titulo");
+    x.style.color = "black";
+    x.style.background = "bisque";
+}
