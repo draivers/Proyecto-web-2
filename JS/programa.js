ff2c24487221770849f881160d1f02ed0049713c
draivers <61165239+draivers@users.noreply.github.com>
2022-03-09 11:11:23 -0500

    k

--- JS/programa.js
+++ JS/programa.js
@@ -10,7 +10,7 @@ function restablecercolor(){
 }
  
 function obtenerpareseimpares(){
-var numero40 = document.getElementById('numero40').valueOf;
+var numero40 = parseInt(document.getElementById('numero40').value);
 if (numero40 < 1000 || numero40 > 9999){
     alert("el numero no es de 4 digitos, intente nuevamente");
 }
